34d67b6c39adedad46c1c61b7a0dfed0952768d9
Mike Mitterer <office@mikemitterer.at>
2016-03-19 17:43:41 +0100

    Poco funkt

--- main.cpp
+++ main.cpp
@@ -1,33 +1,37 @@
-#include <boost/program_options/options_description.hpp>
-#include <boost/program_options.hpp>
+#include "Poco/BasicEvent.h"
+#include "Poco/Delegate.h"
 #include <iostream>
 
-namespace po = boost::program_options;
-using namespace std;
+using Poco::BasicEvent;
+using Poco::Delegate;
 
-// http://www.radmangames.com/programming/how-to-use-boost-program_options
+class Source {
+public:
+    BasicEvent<int> theEvent;
 
-int main(int argc, char *argv[]) {
-// Declare the supported options.
-    po::options_description desc("Allowed options");
-    desc.add_options()
-            ("help", "produce help message")
-            ("compression", po::value<int>(), "set compression level")
-            ;
-
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
-
-    if (vm.count("help")) {
-        cout << desc << "\n";
-        return 1;
+    void fireEvent(int n) {
+        theEvent(this, n);
     }
+};
 
-    if (vm.count("compression")) {
-        cout << "Compression level was set to "
-        << vm["compression"].as<int>() << ".\n";
-    } else {
-        cout << "Compression level was not set.\n";
+class Target {
+public:
+    void onEvent(const void* pSender, int &arg) {
+        std::cout << "onEventt: " << arg << std::endl;
     }
+};
+
+int main(int argc, char** argv) {
+    Source source;
+    Target target;
+
+    source.theEvent += Delegate<Target, int>(
+            &target, &Target::onEvent);
+
+    source.fireEvent(42);
+
+    source.theEvent -= Delegate<Target, int>(
+            &target, &Target::onEvent);
+
+    return 0;
 }
