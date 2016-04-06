c4e69dd632789873c8c4c11fdfd9308ce9b523e2
Mike Mitterer <office@mikemitterer.at>
2016-04-06 20:25:23 +0200

    ID + Daten werden geschickt

--- main.cpp
+++ main.cpp
@@ -6,7 +6,14 @@
 using namespace std;
 
 // Radio pipe addresses for the 2 nodes to communicate.
-const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+// Pipes 1-5 should share the first 32 bits.
+// Only the least significant byte should be unique.
+// You drop the 0x and LL.
+// As stated the 0x indicates the value is a hex value and LL means type Long Long
+const uint64_t pipes[3] = {0xF0F0F0F0E1LL,
+                           0xF0F0F0F0D2LL,
+                           0xF0F0F0F0F3LL
+};
 
 // CE Pin, CSN Pin, SPI Speed
 // Setup for GPIO 22 CE and CE0 CSN with SPI Speed @ 250KBPS
@@ -16,17 +23,19 @@ RF24 radio(RPI_V2_GPIO_P1_22, RPI_V2_GPIO_P1_24, RF24_250KBPS);
 // 1 - write to pipe 1, read from pipe 0
 const int role = 0;
 
-int main(int argc, char** argv)
-{
-    uint8_t len;
+struct MsgToSend {
+    uint16_t id;
+    uint16_t data;
+};
 
+int main(int argc, char** argv) {
     // Refer to RF24.h or nRF24L01 DS for settings
     radio.begin();
     //radio.enableDynamicPayloads();
     radio.setPALevel(RF24_PA_HIGH);
-    radio.setRetries(15,15);
+    radio.setRetries(15, 15);
     radio.setDataRate(RF24_250KBPS);
-    radio.setPayloadSize(8);
+    radio.setPayloadSize(sizeof(MsgToSend));
     radio.setAutoAck(1);
     radio.setChannel(13);
     //radio.setCRCLength(RF24_CRC_16);
@@ -68,36 +77,39 @@ int main(int argc, char** argv)
     delay(50);
 
     radio.startListening();
+    uint16_t dataCounter{};
 
-    while(1)
-    {
-        int16_t receivePayload = 0;
+    while (1) {
         // Start listening
 
-        if(radio.available()) {
-            while ( radio.available() )
-            {
+        if (radio.available()) {
+            MsgToSend dataReceived;
+
+            while (radio.available()) {
                 //len = radio.getDynamicPayloadSize();
-                len = 2;
-                radio.read( &receivePayload, sizeof(receivePayload));
+                const uint16_t len = sizeof(MsgToSend);
+                radio.read(&dataReceived, len);
                 delay(20);
 
-                printf("Recv: size=%i payload=%d pipe=%i",len,receivePayload,1);
-            if(receivePayload == 111) {
-                digitalWrite(pin, 1);
-                delay(250);
-                digitalWrite(pin, 0);
-            }
+                printf("(%5i) Recv: size=%i Data=%i pipe=%i",dataCounter, len, dataReceived.data, dataReceived.id);
+
+                if (dataReceived.data == 111) {
+                    digitalWrite(pin, 1);
+                    delay(250);
+                    digitalWrite(pin, 0);
+                }
 
                 // Send back payload to sender
                 radio.stopListening();
 
                 // if pipe is 7, do not send it back
-                radio.write(&receivePayload,len);
-                printf("\t Send: size=%i payload=%d pipe:%i\n",len,receivePayload,0);
-                receivePayload = 0;
+                radio.write(&dataReceived, len);
+                printf("\t Send: size=%i Data=%i pipe:%i\n", len, dataReceived.data, dataReceived.id);
+                dataReceived.data = 0;
 
                 radio.startListening();
+
+                dataCounter++;
             }
 
         }
